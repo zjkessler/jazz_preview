38c12419a495becfed5403549741c057fccd9043
Zach Kessler <zjkessler@gmail.com>
2017-02-15 21:04:38 -0700

    add server file

---
+++ server.js
@@ -0,0 +1,8 @@
+var express;
+require( 'express' );
+var app = express();
+var mongoose = require( 'mongoose' );
+var path = require( 'path' );
+
+
+app.use( express.static( path.join( __dirname, "app" ) ) );
